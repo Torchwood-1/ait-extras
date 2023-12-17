7c0c20a805a9a32239f85f15e1e6313635b13761
Duzo <james.hall01@outlook.com>
2023-12-17 18:43:09 +0000

    init

---
+++ src/main/java/mc/mdteam/extras/AITExtras.java
@@ -0,0 +1,10 @@
+package mc.mdteam.extras;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AITExtras implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/mc/mdteam/extras/client/AITExtrasClient.java
@@ -0,0 +1,13 @@
+package mc.mdteam.extras.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(value = EnvType.CLIENT)
+public class AITExtrasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
